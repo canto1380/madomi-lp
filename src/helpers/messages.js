9f0d024ea78352bfc4103c3ddc0e7f93af20c4c4
canto1380 <atapenalba16@gmail.com>
2022-07-28 23:53:36 -0300

    cambio en los mensajes

--- src/helpers/messages.js
+++ src/helpers/messages.js
@@ -5,7 +5,7 @@ export const messageSendEmail = () => {
       position: 'center',
       icon: 'success',
       title: 'Mensaje enviado con exito!',
-      html: 'En breve nos pondremos en contacto',
+      html: 'En breve nos pondremos en contacto.',
       showConfirmButton: false,
       timer: 2500
     })
@@ -16,7 +16,7 @@ export const messageSendEmail = () => {
       position: 'center',
       icon: 'warning',
       title: 'A ocurrido un error',
-      html: 'Por favor intentelo de nuevo mas tarde',
+      html: 'Por favor intentelo de nuevo mas tarde.',
       showConfirmButton: false,
       timer: 2500
     })
